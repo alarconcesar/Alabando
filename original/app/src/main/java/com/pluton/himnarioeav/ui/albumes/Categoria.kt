e20ec45a5cb525f1e07c39958634d4f8e2d423d4
cesaralarcon080405 <cesaralarcon080405@gmail.com>
2026-05-21 21:09:14 -0500

    chore: keep only web app for Vercel deployment

--- original/app/src/main/java/com/pluton/himnarioeav/ui/albumes/Categoria.kt
+++
@@ -1,6 +0,0 @@
-package com.pluton.himnarioeav.ui.albumes
-
-data class Categoria(
-    val nombre: String,
-    val cantidad: Int
-)
--- original/app/src/main/java/com/pluton/himnarioeav/ui/favoritos/FavoritosViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.pluton.himnarioeav.ui.favoritos
-
-import androidx.lifecycle.ViewModel
-
-class FavoritosViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
